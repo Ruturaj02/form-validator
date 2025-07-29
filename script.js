fe367fe2f72d8dacfa4f8ed0193546c52868f594
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2025-07-29 18:34:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const form  = document.getElementById("registration-form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+
+form.addEventListener(("submit", function(e){
+    e.preventDefault();
+}))
